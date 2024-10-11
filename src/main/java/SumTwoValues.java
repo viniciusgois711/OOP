aa2c96ef3a55f76479b5e4f536cc7ae301037433
Lucas Vinicius <lu.viniteixeira78@gmail.com>
2024-10-10 21:14:11 -0300

    feat: add question SumTwoValues

---
+++ src/main/java/SumTwoValues.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SumTwoValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valueA = sc.nextInt();
+        int valueB = sc.nextInt();
+        int sum = valueA + valueB;
+        System.out.println("Sum = " + sum);
+    }
+}
