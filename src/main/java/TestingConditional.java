10ebd1ba44d8a383c1e41636e7e27b7f5e90ecbc
Lucas Vinícius Teixeira de Góis <20241014040015@ifrn.local>
2024-10-17 08:31:37 -0300

    Feat: Testing Conditionals

---
+++ src/main/java/TestingConditional.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TestingConditional {
+
+    public static int testingSwitch(int month){
+        return switch(month){
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            default -> 28;
+        };
+    }
+
+    public static int testingIf(int month){
+        if(month == 1 || month == 3 || month == 7 || month == 8 || month == 10 || month == 12){
+            return 31;
+        }else if(month == 4 || month == 6 || month == 9 || month == 11){
+            return 30;
+        }else return 28;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type an integer value between 1 and 12");
+        int month = sc.nextInt();
+
+        System.out.println("Your month have " + testingSwitch(month) + " days");
+        System.out.println("Your month have " + testingIf(month) + " days");
+
+    }
+
+}
