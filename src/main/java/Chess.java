91adad7ed7c3af727ad89fad1582ec4090c8dc80
Lucas Vinícius Teixeira de Góis <20241014040015@ifrn.local>
2024-10-24 08:20:29 -0300

    feat: add chess question

---
+++ src/main/java/Chess.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Chess {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(l%2 != 0 && c%2 != 0 || l%2 == 0 && c%2 == 0){
+            System.out.println("1");
+        }else if(l%2 != 0 && c%2 == 0 || l%2 == 0 && c%2 !=0){
+            System.out.println("0");
+        }
+    }
+}
