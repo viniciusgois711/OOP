2549c7684f66aa22beb3f5405e7cc76ac95c7ca7
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2025-01-23 13:09:39 +0000

    feat: adicionando exercicio4 stream

---
+++ src/main/java/ExercicioStream/Exercicio4.java
@@ -0,0 +1,16 @@
+package ExercicioStream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        List<String> nomes = Arrays.asList("nomeTeste1", "nomeTest2", "nomeTeste3");
+
+        String nomesConcatenados = nomes.stream().collect(Collectors.joining());
+
+        System.out.println(nomesConcatenados);
+    }   
+}
