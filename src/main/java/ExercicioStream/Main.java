13e48b23a7b10a6425894f80daa486132f12246b
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2025-01-23 12:49:17 +0000

    feat: adicionando exercicio1 stream

---
+++ src/main/java/ExercicioStream/Main.java
@@ -0,0 +1,13 @@
+
+package ExercicioStream;
+
+import java.util.Arrays;  // Importa a classe Arrays
+import java.util.List;    // Importa a interface List
+
+public class Main {
+    public static void main(String[] args) {
+       List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        numeros.stream().filter(n -> n%2 == 0).forEach(System.out::println);
+
+    }
+}
